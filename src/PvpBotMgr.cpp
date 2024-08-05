666b5eb2bd67f0bf9c97edfeade55db23a5a8965
Sage <abrahamsage@gmail.com>
2024-08-05 00:03:25 -0400

    idk

--- src/PvpBotMgr.cpp
+++ src/PvpBotMgr.cpp
@@ -363,7 +363,7 @@ uint32 PvpBotMgr::GetEventValue(uint32 bot, std::string const event)
     }
     */
 
-    std::cout << eventName << ":\n";
+    std::cout << event << ":\n";
     std::cout << e.value << "\n";
     std::cout << e.validIn << "\n";
     std::cout << time(0) - e.lastChangeTime << "\n";
