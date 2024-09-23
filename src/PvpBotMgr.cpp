9f4070459d235a53ab5854f927fde65d76ede6f5
Sage <abrahamsage@gmail.com>
2024-09-22 22:44:07 -0400

    try this?

--- src/PvpBotMgr.cpp
+++ src/PvpBotMgr.cpp
@@ -515,20 +515,24 @@ bool PvpBotMgr::ProcessBot(uint32 bot)
     if (player->GetGroup() || player->HasUnitState(UNIT_STATE_IN_FLIGHT))
         return false;
 
-    if (GetEventValue(0, "fraid start") && GetEventValue(bot, "fraid start")) {
+    uint32 gFraidStart = GetEventValue(0, "fraid start");
+    uint32 lFraidStart = GetEventValue(bot, "fraid start");
+    uint32 gFraid = GetEventValue(0, "fraid");
+
+    if (gFraidStart && lFraidStart) {
         std::cout << "adding strategy\n";
         if (GET_PVPPLAYERBOT_AI(player)) {
             GET_PVPPLAYERBOT_AI(player)->SetRaidStrategies();
         }
         SetEventValue(bot, "fraid start", 0, 0);
-    } else if (GetEventValue(0, "fraid")) {
+    } else if (gFraid && !gFraidStart) {
         uint32 zoneId = player->GetZoneId();
-        if (zoneId != 12 && zoneId != 1519 && player->GetLevel() >= 60) {
+        if (player->GetLevel() >= 60) {
             std::cout << "teleporting for raid\n";
             RandomTeleportForLevel(player);
             SetEventValue(bot, "fraid start", 1, 900);
         }
-    }
+    } else
 
     uint32 update = GetEventValue(bot, "update");
     if (!update)
